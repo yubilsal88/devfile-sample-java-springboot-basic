a5a718049b96f6a36d466b849977f539e3aa10e2
Yusuf Bilge SALDAMLI <yubilsal@gmail.com>
2024-04-28 02:26:49 +0300

    add cors policy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @SpringBootApplication
 public class DemoApplication {
